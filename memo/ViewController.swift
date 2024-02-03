9ca1ba7d35f3c7ee50cc52428a6f1d16b4462ca8
ykcho5 <yoonkyoung.cho@visitandregular.com>
2024-02-03 13:41:38 +0900

    Initial Commit

---
+++ memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by 조윤경 on 2024/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
